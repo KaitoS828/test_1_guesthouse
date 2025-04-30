deffaff2cb8cea8fde5c4edcb8c1d8d0c582c2fc
Kaito Sekimoto <kaito.sekimoto8@gmail.com>
2025-04-30 09:55:21 +0900

    設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/test_1_guesthouse/', // 👈 リポジトリ名に置き換えてください
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
